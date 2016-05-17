0494bad10691f6c39b8fd1f3d8161048b33abc12
parknature <parknature1013@hanmanil.net>
2016-05-17 17:19:19 +0900

    register room 수정

--- app/src/main/java/kr/popcorn/sharoom/activity/View/Host/Activity_host_editRoom.java
+++ app/src/main/java/kr/popcorn/sharoom/activity/View/Host/Activity_host_editRoom.java
@@ -2,14 +2,19 @@ package kr.popcorn.sharoom.activity.View.Host;
 
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +25,8 @@ import com.loopj.android.http.RequestParams;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -47,7 +54,19 @@ public class Activity_host_editRoom extends Activity  implements View.OnClickLis
     private ImageButton picButton;
     private ImageButton dialogCam;
     private ImageButton dialogGal;
+
+    private EditText et_title;
+    private EditText et_address;
+    private EditText et_price;
+    private EditText et_roomKind;
+    private EditText et_roomInfo;
+    private EditText et_facilities;
+
     public TextView tv_register;
+    private int mYear, mMonth, mDay;
+    private TextView startDate, endDate;
+    private String start, end;
+    private String today;
     private TextView toptext;
 
     @Override
@@ -80,6 +99,196 @@ public class Activity_host_editRoom extends Activity  implements View.OnClickLis
         mImagePicker = new ImagePicker(this);
         loadData();
 
+        et_title = (EditText)findViewById(R.id.et_title);
+        et_address = (EditText)findViewById(R.id.et_address);
+        et_price = (EditText)findViewById(R.id.et_price);
+        et_roomKind = (EditText)findViewById(R.id.et_roomKind);
+        et_roomInfo = (EditText) findViewById(R.id.et_roominfo);
+        et_facilities = (EditText)findViewById(R.id.et_facilities);
+
+        et_title.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                                              @Override
+                                              public void onFocusChange(View v, boolean hasFocus) {
+                                                  if (hasFocus) {
+                                                      et_title.setHint("");
+                                                      InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                                                      imm.showSoftInput(et_title, InputMethodManager.SHOW_IMPLICIT);
+                                                  } else
+                                                      et_title.setHint("주말 빌려드립니다! 연락주세요!");
+                                              }
+                                          }
+        );
+
+        et_address.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et_address.setHint("");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(et_address, InputMethodManager.SHOW_IMPLICIT);
+
+                } else
+                    et_address.setHint("서울시 성북구 정릉동 11-12");
+            }
+        });
+
+
+        et_price.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et_price.setHint("");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(et_price, InputMethodManager.SHOW_IMPLICIT);
+
+                } else
+                    et_price.setHint("300,000");
+            }
+        });
+        et_roomKind.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et_roomKind.setHint("");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(et_roomKind, InputMethodManager.SHOW_IMPLICIT);
+
+                } else
+                    et_roomKind.setHint("원룸, 자취방, 하숙집...etc");
+            }
+        });
+
+
+        et_facilities.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et_facilities.setHint("");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(et_facilities, InputMethodManager.SHOW_IMPLICIT);
+
+                } else
+                    et_address.setHint("침대 1인용, 컴퓨터 책상, 옷걸이...etc");
+            }
+        });
+        et_roomInfo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et_roomInfo.setHint("");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(et_roomInfo, InputMethodManager.SHOW_IMPLICIT);
+
+                } else
+                    et_roomInfo.setHint("국민대학교 정문에서 걸어서 10분정도 걸리는 거리에 위치한 원룸입니다. 2달 정도 방을 비워야 할일이 생겨서 같은 학교 학생분께 저렴한 가격에 대여해드리고 싶습니다. 연락주세요.");
+            }
+        });
+
+        et_title.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_title.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+        et_address.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_address.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+        et_price.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_price.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        et_roomKind.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_roomKind.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+        et_facilities.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_facilities.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+        et_roomInfo.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(et_roomInfo.getWindowToken(), 0);    //hide keyboard
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        startDate = (TextView) findViewById(R.id.startDate);
+        endDate = (TextView) findViewById(R.id.endDate);
+
+        Calendar cal = new GregorianCalendar();
+        mYear = cal.get(Calendar.YEAR);
+        mMonth = cal.get(Calendar.MONTH);
+        mDay = cal.get(Calendar.DAY_OF_MONTH);
+        //날짜 초기값 현재날짜로 세팅
+        today = String.format("%d/%d/%d", mYear, mMonth+1, mDay);
+        startDate.setText(String.format("%d/%d/%d", mYear, mMonth+1, mDay));
+        endDate.setText(String.format("%d/%d/%d", mYear, mMonth + 1, mDay));
+
+        //달력 입력을 받기 위한 다이얼로그
+        startDate.setOnClickListener(new TextView.OnClickListener() { //시작날짜
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.startDate:
+                        new DatePickerDialog(Activity_host_editRoom.this, mDateSetListener1, mYear, mMonth, mDay).show();
+                        break;
+
+                }
+            }
+        });
+
+        endDate.setOnClickListener(new TextView.OnClickListener() { //끝 날짜
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.endDate:
+                        new DatePickerDialog(Activity_host_editRoom.this, mDateSetListener2, mYear, mMonth, mDay).show();
+                        break;
+
+                }
+            }
+        });
+
         tv_register = (TextView) findViewById(R.id.bottomtext);
         tv_register.setOnClickListener(new TextView.OnClickListener(){
             public void onClick(View v) {
@@ -92,6 +301,34 @@ public class Activity_host_editRoom extends Activity  implements View.OnClickLis
         });
 
     }
+    DatePickerDialog.OnDateSetListener mDateSetListener1 =
+            new DatePickerDialog.OnDateSetListener(){
+
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+
+                    startDate.setText(String.format("%d/%d/%d", mYear, mMonth+1, mDay));
+                    start = new String(String.valueOf(mYear)+"/"+String.valueOf(mMonth+1)+"/"+String.valueOf(mDay));
+                }
+            };
+
+    DatePickerDialog.OnDateSetListener mDateSetListener2 =
+            new DatePickerDialog.OnDateSetListener(){
+
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+
+                    endDate.setText(String.format("%d/%d/%d", mYear, mMonth+1, mDay));
+                    end = new String(String.valueOf(mYear)+"/"+String.valueOf(mMonth+1)+"/"+String.valueOf(mDay));
+
+                }
+            };
 
     @Override
     public void onClick(View v) {
